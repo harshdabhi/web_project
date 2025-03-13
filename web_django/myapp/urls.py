99eae3f2a509dd0d17ecf58b35bb0d39484aa9c1
Raskc <edgar.dion@epitech.eu>
2025-03-13 01:41:37 +0000

    Set u URL routing for API endpoints

--- web_django/myapp/urls.py
+++ web_django/myapp/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
 from . import views
 
-# Just add pages routes here 
-
 urlpatterns=[
-    # path to home page
     path("",views.home,name='home'),
-
-    # path to about us page
     path("about",views.about,name='about'),
-
-    
 ]
--- web_django/myapp/views.py
+++ web_django/myapp/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render,HttpResponse
 
-
-# Create your views here.
-
 def home(request):
     return render(request,"home.html")
-
-
 
 def about(request):
     return render(request,"about.html")
     
+def api_docs(request):
+    return render(request, "api_docs.html")
+
 
