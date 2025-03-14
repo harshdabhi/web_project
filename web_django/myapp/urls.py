68d434a52d0c98514a2971a4692d6c335e079289
AstridDcu <astrid.blonce2@mail.dcu.ie>
2025-03-14 08:53:23 +0000

    Update urls.py

--- web_django/myapp/urls.py
+++ web_django/myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path("",views.home,name='home'),
     path("about",views.about,name='about'),
-    path("login/",views.login,name='login'),
+    path("login",views.login,name='login'),
     path("contact",views.contact,name='contact'),
     path("faqs",views.faqs,name='faqs'),
 
