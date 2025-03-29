16dbe9ce9aebe9d9103615434d384a00eff57ebb
AstridDcu <astrid.blonce2@mail.dcu.ie>
2025-03-29 06:58:41 +0000

    Update urls.py

--- web_django/myapp/urls.py
+++ web_django/myapp/urls.py
@@ -12,4 +12,11 @@ urlpatterns=[
     path("dashboardrepair",views.dashboardrepair,name='dashboardrepair'),
     path("dashboardtechnician",views.dashboardtechnician,name='dashboardtechnician'),
     path("dashboardmanager",views.dashboardmanager,name='dashboardmanager'),
+
+    path("machine1",views.machine1,name='machine1'),
+    path("machine2",views.machine2,name='machine2'),
+    path("machine3",views.machine3,name='machine3'),
+    path("machine4",views.machine4,name='machine4'),
+    path("machine5",views.machine5,name='machine5'),
+    path("machine6",views.machine6,name='machine6'),
 ]
